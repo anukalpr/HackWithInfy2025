bdea32314588d9030a7ab2310ba43fa8f15fc195
Anukalp Raj <122322905+anukalpr@users.noreply.github.com>
2025-07-03 09:54:43 +0530

    Add files via upload

---
+++ GreedyAlgo/ArrayPartitions.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayPartitions {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int sum=0;
+        for(int i=0; i<arr.length; i+=2){
+            sum+=arr[i];
+        }
+        System.out.println(sum);
+    }
+}
